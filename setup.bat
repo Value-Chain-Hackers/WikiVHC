43aa05ac51a7be6e21228aa95cb0f7e78b1d8887
Christiaan Verhoef <christiaan_gerardo@hotmail.com>
2024-06-27 19:22:27 +0200

    update Wiki
    Mermaid?

---
+++ setup.bat
@@ -0,0 +1,112 @@
+@echo off
+mkdir docs
+
+(
+echo ---
+echo title: "Welcome to Value Chain Hackers"
+echo ---
+echo.
+echo # Welcome
+echo.
+echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+) > docs\index.qmd
+
+(
+echo ---
+echo title: "Introduction"
+echo ---
+echo.
+echo # Introduction
+echo.
+echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+) > docs\intro.qmd
+
+(
+echo ---
+echo title: "Why"
+echo ---
+echo.
+echo # Why
+echo.
+echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+) > docs\why.qmd
+
+(
+echo ---
+echo title: "How"
+echo ---
+echo.
+echo # How
+echo.
+echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+) > docs\how.qmd
+
+(
+echo ---
+echo title: "What"
+echo ---
+echo.
+echo # What
+echo.
+echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+) > docs\what.qmd
+
+(
+echo ---
+echo title: "Tools"
+echo ---
+echo.
+echo # Tools
+echo.
+echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+) > docs\tools.qmd
+
+(
+echo ---
+echo title: "Student Flow"
+echo ---
+echo.
+echo # Student Flow
+echo.
+echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+) > docs\student-flow.qmd
+
+(
+echo ---
+echo title: "Roles"
+echo ---
+echo.
+echo # Roles
+echo.
+echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+) > docs\roles.qmd
+
+(
+echo ---
+echo title: "Resources"
+echo ---
+echo.
+echo # Resources
+echo.
+echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+) > docs\resources.qmd
+
+(
+echo ---
+echo title: "Summary"
+echo ---
+echo.
+echo # Summary
+echo.
+echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+) > docs\summary.qmd
+
+(
+echo ---
+echo title: "References"
+echo ---
+echo.
+echo # References
+echo.
+echo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+) > docs\references.qmd
